15811f32336089ff908203a0c1fb2c1d26a57d7f
ArtemNozhenko <artem2516nozh@gmail.com>
2024-05-27 23:29:21 +0200

    add

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -11,15 +11,52 @@ export default function MovieDetailsPage() {
       try {
         const date = await fetchMovieDetails(movieId);
         setMovieDetails(date);
-        console.log(date);
       } catch {
         console.log("error!");
       }
     };
-    getMovieDetails;
+    getMovieDetails();
   }, [movieId]);
 
-  console.log(movieDetails);
+  const {
+    poster_path,
+    original_title,
+    overview,
+    genres,
+    vote_average,
+  } = movieDetails;
+  const defaultImg =
+    "<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>";
+  const votePercentage = (vote_average * 10).toFixed(1);
 
-  return;
+  return (
+    <div>
+      <button>Go back</button>
+      <div>
+        <img
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+              : defaultImg
+          }
+          width={250}
+          alt="poster"
+        />
+        <div>
+          <h2>{original_title}</h2>
+          <p>User Score {votePercentage}%</p>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          <h3>Genres</h3>
+          <ul>
+            {genres &&
+              genres.length > 0 &&
+              genres.map(({ id, name }) => (
+                <li key={id}>{name}</li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
